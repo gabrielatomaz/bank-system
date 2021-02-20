2e869a0a94676b42e5590302eeb862371ca65398
Gabriela Tomaz <gabrielatomaz1772@gmail.com>
2021-02-20 16:55:49 -0300

    started front app

---
+++ front/src/components/Banner.vue
@@ -0,0 +1,22 @@
+
+<template>
+  <section class="hero is-primary">
+    <div class="hero-body">
+      <p class="title">
+        {{ title }}
+      </p>
+      <p class="subtitle mt-3">
+        <slot></slot>
+      </p>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "Banner",
+  props: {
+    title: String,
+  },
+};
+</script>
---
+++ front/src/views/Index.vue
@@ -0,0 +1,20 @@
+<template>
+  <Banner title="Wellcome!"> </Banner>
+</template>
+
+<script>
+import { Banner } from '../components'
+
+export default {
+    name: 'Index',
+
+    components: {
+        Banner,
+    },
+
+}
+</script>
+
+<style>
+
+</style>
